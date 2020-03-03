3b36b2b4ae897793cce2d0bb1ad3c1d028506296
Ralph Schindler <ralph@ralphschindler.com>
2020-03-03 14:47:03 -0600

    fix for index vue template

--- resources/js/components/IndexField.vue
+++ resources/js/components/IndexField.vue
@@ -2,14 +2,14 @@
   <div>
     <componet
       v-bind:is="field.reorderable == true ? 'ol' : 'ul'"
-      v-if="Array.isArray(field.value_for_detail_display)"
+      v-if="Array.isArray(field.value_for_index_display)"
       >
-      <li v-for="item in field.value_for_detail_display">
+      <li v-for="item in field.value_for_index_display">
         {{ item }}
       </li>
     </componet>
-    <span v-if="typeof field.value_for_detail_display == 'string'">
-      {{ field.value_for_detail_display || '-'}}
+    <span v-if="typeof field.value_for_index_display == 'string'">
+      {{ field.value_for_index_display || '-'}}
     </span>
   </div>
 </template>
