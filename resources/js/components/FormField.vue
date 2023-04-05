687232c8b7be4c742a70f8c7334a87980580a992
Justin <2125752+justindantzer@users.noreply.github.com>
2023-04-05 10:09:07 -0400

    Improvements to form field style (#56)
    * Updated open-indicator to match other Nova fields
    
    * Updated input styles to better match other Nova 4 inputs

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -16,6 +16,21 @@
           @option:selected="$emit('field-changed')"
           @option:deselected="$emit('field-changed')"
         >
+          <template #open-indicator="{ attributes }">
+            <svg
+              v-bind="attributes"
+              class="flex-shrink-0 pointer-events-none form-select-arrow"
+              xmlns="http://www.w3.org/2000/svg"
+              width="10"
+              height="6"
+              viewBox="0 0 10 6"
+            >
+              <path
+                class="fill-current"
+                d="M8.292893.292893c.390525-.390524 1.023689-.390524 1.414214 0 .390524.390525.390524 1.023689 0 1.414214l-4 4c-.390525.390524-1.023689.390524-1.414214 0l-4-4c-.390524-.390525-.390524-1.023689 0-1.414214.390525-.390524 1.023689-.390524 1.414214 0L5 3.585786 8.292893.292893z"
+              ></path>
+            </svg>
+          </template>
           <template #no-options>
             <span v-if="field.isAjaxSearchable">
               Type to search...
